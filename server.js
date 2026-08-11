0209756525595c00eb03e0b0790a86bcaee469f0
Kosisochukwu Emmanuel <kosiudeh627@gmail.com>
2026-08-11 20:59:38 +0100

    fix cors and json middleware

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: [
+    'http://localhost:3000',
+    'https://multi-tenant-ecommerce-nine.vercel.app',
+    process.env.CLIENT_URL
+  ].filter(Boolean),
   credentials: true
 }));
 
